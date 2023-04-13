9ce5404501c29cacbbb665a8670e56940e3667c6
Abuzar <abuzar28102004@gmail.com>
2023-04-13 19:25:07 +0300

    feat: add initial setup for project

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship Game',
+      favicon: './src/images/battleship.svg',
+    }),
+  ],
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    start: '/dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  mode: 'development',
+
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+}
