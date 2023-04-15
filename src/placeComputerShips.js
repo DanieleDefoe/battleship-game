53330528bd5560c133106b5a8c064247fc42132c
Abuzar <abuzar28102004@gmail.com>
2023-04-15 14:45:26 +0300

    feat: create full game

--- src/placeComputerShips.js
+++ src/placeComputerShips.js
@@ -1,57 +1,54 @@
 /* eslint-disable no-continue */
 /* eslint-disable no-sequences */
-import Ship from './shipClass'
+import { ships } from './shipClass'
+
+const computerShips = [...ships]
 
 const placeComputerShips = (computer) => {
-  const carrier = new Ship('Carrier', 5)
-  const battleship = new Ship('Battleship', 4)
-  const destroyer = new Ship('Destroyer', 3)
-  const submarine = new Ship('Submarine', 3)
-  const patrolBoat = new Ship('Patrol-boat', 2)
-
-  const ships = [carrier, battleship, destroyer, submarine, patrolBoat]
-
-  for (let i = 0; i < ships.length; i += 1) {
-    const currentShip = ships[i]
+  if (computerShips.length > 0) {
+    const randomStartIndex = Math.floor(Math.random() * 100)
+    const currentShip = computerShips[0]
     const { length } = currentShip
     const isHorizontal = Math.random() < 0.5
-
-    const randomStartIndex = Math.floor(Math.random() * 100)
     if ((randomStartIndex + length - 1) % 10 < length - 1 && isHorizontal) {
-      i -= 1
-      continue
+      placeComputerShips(computer)
+      return
     }
     if ((randomStartIndex + length * 10) % 100 < length * 10 && !isHorizontal) {
-      i -= 1
-      continue
-    }
-    if (
-      computer
-        .querySelector(`div[id="${randomStartIndex}"]`)
-        .classList.contains('black-ship')
-    ) {
-      i -= 1
-      continue
+      placeComputerShips(computer)
+      return
     }
 
+    const shipNodes = []
+
     for (
-      let j = randomStartIndex, k = 0;
-      j <= randomStartIndex + length, k < length;
-      j += 1, k += 1
+      let i = randomStartIndex, j = 0;
+      i <= randomStartIndex + length, j < length;
+      i += 1, j += 1
     ) {
       let node
       if (isHorizontal) {
-        node = computer.querySelector(`div[id="${j}"]`)
+        node = computer.querySelector(`div[id="${i}"]`)
       } else {
-        node = computer.querySelector(`div[id="${j + k * 10}"]`)
-        j -= 1
-      }
-      if (node.classList.contains('black-ship')) {
+        node = computer.querySelector(`div[id="${i + j * 10}"]`)
         i -= 1
-        continue
       }
-      node.classList.add('black-ship', currentShip.name)
+
+      if (node.classList.contains('taken')) {
+        placeComputerShips(computer)
+        return
+      }
+      shipNodes.push(node)
     }
+
+    if (shipNodes.length === length) {
+      for (let i = 0; i < shipNodes.length; i += 1) {
+        shipNodes[i].classList.add('taken', currentShip.name)
+      }
+    }
+
+    computerShips.shift()
+    placeComputerShips(computer)
   }
 }
 
